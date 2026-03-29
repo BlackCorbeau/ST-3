745f1a7b1f21d454fdec781705b5d035139ff5a0
Kirill <remizov.kirill2005@yandex.ru>
2026-03-29 19:02:25 +0300

    feat(src): Add realization

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,52 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
+#include <thread>
+#include <chrono>
+#include <stdexcept>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+
+void DoorTimerAdapter::Timeout() {
+    door.throwState();
+}
+
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
+    adapter = new DoorTimerAdapter(*this);
+}
+
+bool TimedDoor::isDoorOpened() {
+    return isOpened;
+}
+
+void TimedDoor::unlock() {
+    if (!isOpened) {
+        isOpened = true;
+        Timer t;
+        t.tregister(iTimeout, adapter);
+    }
+}
+
+void TimedDoor::lock() {
+    isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+    return iTimeout;
+}
+
+void TimedDoor::throwState() {
+    if (isOpened) {
+        throw std::runtime_error("Door timeout: still open");
+    }
+}
+
+void Timer::sleep(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+
+void Timer::tregister(int timeout, TimerClient* client) {
+    std::thread([timeout, client]() {
+        std::this_thread::sleep_for(std::chrono::seconds(timeout));
+        client->Timeout();
+    }).detach();
+}
