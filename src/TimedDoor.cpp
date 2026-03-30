49000c594e6d5e95b96f82e5d6a8dfbb8791ff3e
Kirill <remizov.kirill2005@yandex.ru>
2026-03-30 09:19:24 +0300

    fix: Clang-tidy

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,52 +1,44 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
-#include <thread>
 #include <chrono>
 #include <stdexcept>
+#include <thread>
 
-DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor &d) : door(d) {}
 
-void DoorTimerAdapter::Timeout() {
-    door.throwState();
-}
+void DoorTimerAdapter::Timeout() { door.throwState(); }
 
 TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
-    adapter = new DoorTimerAdapter(*this);
+  adapter = new DoorTimerAdapter(*this);
 }
 
-bool TimedDoor::isDoorOpened() {
-    return isOpened;
-}
+bool TimedDoor::isDoorOpened() { return isOpened; }
 
 void TimedDoor::unlock() {
-    if (!isOpened) {
-        isOpened = true;
-        Timer t;
-        t.tregister(iTimeout, adapter);
-    }
+  if (!isOpened) {
+    isOpened = true;
+    Timer t;
+    t.tregister(iTimeout, adapter);
+  }
 }
 
-void TimedDoor::lock() {
-    isOpened = false;
-}
+void TimedDoor::lock() { isOpened = false; }
 
-int TimedDoor::getTimeOut() const {
-    return iTimeout;
-}
+int TimedDoor::getTimeOut() const { return iTimeout; }
 
 void TimedDoor::throwState() {
-    if (isOpened) {
-        throw std::runtime_error("Door timeout: still open");
-    }
+  if (isOpened) {
+    throw std::runtime_error("Door timeout: still open");
+  }
 }
 
 void Timer::sleep(int seconds) {
-    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
 }
 
-void Timer::tregister(int timeout, TimerClient* client) {
-    std::thread([timeout, client]() {
-        std::this_thread::sleep_for(std::chrono::seconds(timeout));
-        client->Timeout();
-    }).detach();
+void Timer::tregister(int timeout, TimerClient *client) {
+  std::thread([timeout, client]() {
+    std::this_thread::sleep_for(std::chrono::seconds(timeout));
+    client->Timeout();
+  }).detach();
 }
